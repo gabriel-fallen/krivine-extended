45ed85546d9e89511910857523e91de7a0031aac
Alexander Tchitchigin <sad.ronin@gmail.com>
2018-06-27 18:16:32 +0300

    More large terms for benchmarking. Now without printing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.DeepSeq
 import Lib
 
 
@@ -36,6 +37,9 @@ plus = Lam 4 $ App (Var 0 0) [Var 0 2, App (Var 0 1) [Var 0 2, Var 0 3]]
 add21 :: Term
 add21 = App plus [c2, c1, Lam 1 $ App (Free "1+") [Var 0 0], Free "0"]
 
+add21' :: Term
+add21' = App plus [c2, c1]
+
 -- Perf test with large expressions
 
 idt :: Term
@@ -47,18 +51,36 @@ nid = (!!) $ iterate (App idt . (: [])) (Free "x")
 testt :: Int -> Int -> Term
 testt n m = App (Free "z") $ replicate m $ nid n
 
+nmpair :: Int -> Int -> Term
+nmpair n m = App t [u n, u m]
+  where
+    -- t = \x.\y.((a)(x)y)(b)(y)x
+    t = Lam 2 $ App (Free "a") [App (Var 0 0) [Var 0 1], App (Free "b") [App (Var 0 1) [Var 0 0]]]
+    -- u k = \f.\z.(f)^{k}z -- Church numeral @k@
+    u k = Lam 2 $ iterate (App (Var 0 0) . (: [])) (Var 0 1) !! k
+
+klmn :: Int -> Int -> Int -> Int -> Term
+klmn k l m n = App t [u k, u l, u m, u n]
+  where
+    -- t = \x1.\x2.\x3.\x4.((((a)(x1)x2) (b)(x2)x1) (c)(x3)x4) (d)(x4)x3
+    t = Lam 4 $ App (Free "a") [App (Var 0 0) [Var 0 1], App (Free "b") [App (Var 0 1) [Var 0 0]], App (Free "c") [App (Var 0 2) [Var 0 3]], App (Free "d") [App (Var 0 3) [Var 0 2]]]
+    -- u k = \f.\z.(f)^{k}z -- Church numeral @k@
+    u k = Lam 2 $ iterate (App (Var 0 0) . (: [])) (Var 0 1) !! k
+    
 
 main :: IO ()
 main = do
-  let end = App (Free "end") []
-      r1  = eval' term1
-      r2  = eval' term2
-      r3  = eval' term3
-      r4  = eval' term4
-      r5  = eval' term5
-      r21 = eval' add21
-      small = eval' $ testt 7 6
-      big   = eval' $ testt 15000 2000
+  let end    = App (Free "end") []
+      r1     = eval' term1
+      r2     = eval' term2
+      r3     = eval' term3
+      r4     = eval' term4
+      r5     = eval' term5
+      r21    = eval' add21
+      r21'   = eval' add21'
+      big    = eval' $ testt 15000 2000
+      pair57 = eval' $ nmpair 5 7
+      klmn5766 = eval' $ klmn 5 7 6 6
   -- print r1
   -- putStrLn $ if r1 == end then "pass" else "fail"
   -- print r2
@@ -67,5 +89,9 @@ main = do
   -- putStrLn $ if r3 == end then "pass" else "fail"
   -- print r4
   -- print r21
+  -- print r21'
   -- print r5
-  print big
+  -- print big
+  -- print pair56
+  -- pair57 `deepseq` putStrLn "Done."
+  klmn5766 `deepseq` putStrLn "Done."
